65934c0b9055681f30efb8e35fad84653fc1db2c
nise.moe <162507023+nise-moe@users.noreply.github.com>
2024-03-08 08:15:53 +0100

    added backwards compatible serializer for time value

--- src/main/kotlin/org/nisemoe/mari/judgements/JudgementModel.kt
+++ src/main/kotlin/org/nisemoe/mari/judgements/JudgementModel.kt
@@ -1,7 +1,32 @@
 package org.nisemoe.mari.judgements
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlin.math.roundToInt
+
+/**
+ * Backwards compatibility with time values that were persisted as doubles.
+ */
+object TimeSerializer : KSerializer<Int> {
+
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Time", PrimitiveKind.INT)
+
+    override fun serialize(encoder: Encoder, value: Int) {
+        encoder.encodeInt(value)
+    }
+
+    override fun deserialize(decoder: Decoder): Int {
+        val doubleValue = decoder.decodeDouble()
+        return doubleValue.roundToInt()
+    }
+
+}
 
 /**
  * Represents a judgement on a hit object.
@@ -9,7 +34,9 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 data class Judgement(
+    @Serializable(with = TimeSerializer::class)
     val time: Int,
+
     val x: Double,
     val y: Double,
     val type: Type,
